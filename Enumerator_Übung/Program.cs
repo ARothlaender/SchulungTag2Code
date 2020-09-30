8c2ab7be2c7cc6c33763781aaa89916e1a2abbea
ARothlaender <72085124+ARothlaender@users.noreply.github.com>
2020-09-30 15:44:11 +0200

    123

---
+++ Enumerator_Übung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enumerator_Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
