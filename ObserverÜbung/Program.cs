1a8de9d6a006766b96e5dac19436c5a582740271
ARothlaender <72085124+ARothlaender@users.noreply.github.com>
2020-09-30 10:27:22 +0200

    HInzufügen

---
+++ ObserverÜbung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverÜbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
